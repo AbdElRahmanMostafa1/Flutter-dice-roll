69734055e85101241af8cc2148f9d55dc9f735fe
AbdElRahman Mostafa <abdelrahman22.mostafa@gmail.com>
2023-11-17 22:58:10 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() {
+  const text = 'TEST';
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.deepPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28.0,
+            ),
+          ),
+        ),
+      ),
+    ),
+  ));
+}
